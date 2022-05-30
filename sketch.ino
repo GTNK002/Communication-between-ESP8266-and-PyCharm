b73fc6ab2dcc4c8f23ecfac26fe35c1dbf1c433e
Roboscience GTNK <79186183+GTNK002@users.noreply.github.com>
2022-05-30 10:44:01 +0530

    Add files via upload

---
+++ sketch.ino
@@ -0,0 +1,40 @@
+String command ;
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  Serial.begin(9600);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (Serial.available() > 0) {
+
+  command = Serial.readStringUntil('\n');
+
+    if (command == "on") {
+    
+      digitalWrite(LED_BUILTIN, LOW);
+
+      Serial.write("Led on");
+    }
+
+    else if (command == "off") {
+
+      digitalWrite(LED_BUILTIN, HIGH);
+
+      Serial.write("Led off");
+    }
+
+    else{
+
+      Serial.write("Invaild command");
+
+    }
+    
+  }
+}
